46410a6f96c8d4a9b0d890fa3c3d3363d92eee19
1127071993@qq.com <aini2009626>
2022-09-11 17:57:05 +0800

    feat:02-编写Threejs项目基础代码

--- autoshow/src/App.vue
+++ autoshow/src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="home">
+    <div class="canvas-container" ref="canvasDom"></div>
+ </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import * as THREE from "three";
+import { onMounted, reactive, ref } from "vue";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+let controls;
+let canvasDom = ref(null);
+// 创建场景
+const scene = new THREE.Scene();
+// 创建相机
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+// 设置相机位置
+camera.position.set(0, 2, 6);
+// 创建渲染器
+const renderer = new THREE.WebGLRenderer({
+  // 抗锯齿
+  antialias: true,
+});
+// 设置渲染器大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 渲染函数
+const render = () => {
+  renderer.render(scene, camera);
+  // 控制器可以不断 更新视角
+  controls && controls.update();
+  requestAnimationFrame(render);
+};
+
+onMounted(()=>{
+  // 把渲染器插入Dom中
+  canvasDom.value.appendChild(renderer.domElement);
+   // 初始化渲染器，渲染背景
+  renderer.setClearColor("#000");
+  //背景色
+  scene.background = new THREE.Color("#ccc");
+  //环境
+  scene.environment = new THREE.Color("#ccc");
+  render();
+
+  // 添加网格地面
+  const gridHelper = new THREE.GridHelper(10, 10);
+  gridHelper.material.opacity = 0.2; //透明度
+  gridHelper.material.transparent = true; //开启透明
+  scene.add(gridHelper);
+
+  // 添加控制器
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+})
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
