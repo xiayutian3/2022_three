6ec5aa5fe1fa68e86e38181d3551eeb51cc845ed
1127071993@qq.com <aini2009626>
2022-09-12 08:10:56 +0800

    feat：04-选配材质与物理材质设置

--- autoshow/src/App.vue
+++ autoshow/src/App.vue
@@ -1,6 +1,38 @@
 <template>
  <div class="home">
     <div class="canvas-container" ref="canvasDom"></div>
+
+     <div class="home-content">
+      <div class="home-content-title">
+        <h1>汽车展示与选配</h1>
+      </div>
+      <h2>选择车身颜色</h2>
+      <div class="select">
+        <div
+          class="select-item"
+          v-for="(item, index) in colors"
+          :key="index"
+          @click="selectColor(index)"
+        >
+          <div
+            class="select-item-color"
+            :style="{ backgroundColor: item }"
+          ></div>
+        </div>
+      </div>
+
+      <h2>选择贴膜材质</h2>
+      <div class="select">
+        <div
+          class="select-item"
+          v-for="(item, index) in materials"
+          :key="index"
+          @click="selectMaterial(index)"
+        >
+          <div class="select-item-text">{{ item.name }}</div>
+        </div>
+      </div>
+    </div>
  </div>
 </template>
 
@@ -8,7 +40,10 @@
 import * as THREE from "three";
 import { onMounted, reactive, ref } from "vue";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
+// gltf模型加载loader
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// 解压缩工具，因为模型导出的时候压缩了
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 let controls;
 let canvasDom = ref(null);
@@ -39,6 +74,66 @@ const render = () => {
   requestAnimationFrame(render);
 };
 
+let wheels = [];
+let carBody, frontCar, hoodCar, glassCar;
+
+// 创建材质(物理材质)
+const bodyMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xff0000, //车身颜色
+  metalness: 1, //金属度
+  roughness: 0.5, //粗糙程度
+  clearcoat: 1, //设置清漆,像汽车喷漆的光滑效果
+  clearcoatRoughness: 0, //设置清漆粗糙度，想要比较光滑，设置为0
+});
+const frontMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xff0000,
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+const hoodMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xff0000,
+  metalness: 1,
+  roughness: 0.5,
+  clearcoat: 1,
+  clearcoatRoughness: 0,
+});
+const wheelsMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xff0000,
+  metalness: 1,
+  roughness: 0.1,
+});
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  color: 0xffffff, //颜色
+  metalness: 0,//金属度
+  roughness: 0, //粗糙程度
+  transmission: 1, //设置透光率,完全透过去
+  transparent: true, //是否透明
+});
+
+//颜色选择
+let colors = ["red", "blue", "green", "gray", "orange", "purple"];
+let selectColor = (index) => {
+  bodyMaterial.color.set(colors[index]);
+  frontMaterial.color.set(colors[index]);
+  hoodMaterial.color.set(colors[index]);
+  wheelsMaterial.color.set(colors[index]);
+  // glassMaterial.color.set(colors[index]);
+};
+
+//粗糙度方面（贴膜的粗糙程度）
+let materials = [
+  { name: "磨砂", value: 1 },
+  { name: "冰晶", value: 0 },
+];
+let selectMaterial = (index) => {
+  bodyMaterial.clearcoatRoughness = materials[index].value;
+  frontMaterial.clearcoatRoughness = materials[index].value;
+  hoodMaterial.clearcoatRoughness = materials[index].value;
+};
+
+
 onMounted(()=>{
   // 把渲染器插入Dom中
   canvasDom.value.appendChild(renderer.domElement);
@@ -59,6 +154,81 @@ onMounted(()=>{
   // 添加控制器
   controls = new OrbitControls(camera, renderer.domElement);
   controls.update();
+
+  // 添加gltf汽车模型
+  const loader = new GLTFLoader();
+  const dracoLoader = new DRACOLoader(); // 解压缩工具
+  dracoLoader.setDecoderPath("./draco/gltf/"); // 设置gltf解压缩工具路径
+  loader.setDRACOLoader(dracoLoader); //添加到loader中
+  // 解压文件
+  loader.load("./model/bmw01.glb", (gltf) => {
+    //获得内容
+    const bmw = gltf.scene;
+    // console.log('bmw: ', bmw);
+    bmw.traverse(child => {
+      // 判断是不是物体，因为每个物体创建的时候都设置了名字
+      if (child.isMesh) {
+        console.log(child.name)
+      }
+       // 判断是否是轮毂
+      if (child.isMesh && child.name.includes("轮毂")) {
+        child.material = wheelsMaterial;
+        wheels.push(child);
+      }
+      // 判断是否是车身
+      if (child.isMesh && child.name.includes("Mesh002")) {
+        carBody = child;
+        carBody.material = bodyMaterial;
+      }
+      // 判断是否是前脸
+      if (child.isMesh && child.name.includes("前脸")) {
+        child.material = frontMaterial;
+        frontCar = child;
+      }
+      // 判断是否是引擎盖
+      if (child.isMesh && child.name.includes("引擎盖_1")) {
+        child.material = hoodMaterial;
+        hoodCar = child;
+      }
+      // 判断是否是挡风玻璃
+      if (child.isMesh && child.name.includes("挡风玻璃")) {
+        child.material = glassMaterial;
+        glassCar = child;
+      }
+    })
+    scene.add(bmw); //添加到场景中
+  })
+
+
+  // 添加灯光(物理材质需要打灯)
+  const light1 = new THREE.DirectionalLight(0xffffff, 1);
+  light1.position.set(0, 0, 10);
+  scene.add(light1);
+  const light2 = new THREE.DirectionalLight(0xffffff, 1);
+  light2.position.set(0, 0, -10);
+  scene.add(light2);
+  const light3 = new THREE.DirectionalLight(0xffffff, 1);
+  light3.position.set(10, 0, 0);
+  scene.add(light3);
+  const light4 = new THREE.DirectionalLight(0xffffff, 1);
+  light4.position.set(-10, 0, 0);
+  scene.add(light4);
+  const light5 = new THREE.DirectionalLight(0xffffff, 1);
+  light5.position.set(0, 10, 0);
+  scene.add(light5);
+  const light6 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light6.position.set(5, 10, 0);
+  scene.add(light6);
+  const light7 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light7.position.set(0, 10, 5);
+  scene.add(light7);
+  const light8 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light8.position.set(0, 10, -5);
+  scene.add(light8);
+  const light9 = new THREE.DirectionalLight(0xffffff, 0.3);
+  light9.position.set(-5, 10, 0);
+  scene.add(light9);
+
 })
 </script>
 
@@ -66,5 +236,23 @@ onMounted(()=>{
 * {
   margin: 0;
   padding: 0;
+}
+.home-content {
+  position: fixed;
+  top: 0;
+  right: 20px;
+}
+
+.select-item-color {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #ccc;
+  margin: 10px;
+  display: inline-block;
+  cursor: pointer;
+  border-radius: 10px;
+}
+.select {
+  display: flex;
 }
 </style>
